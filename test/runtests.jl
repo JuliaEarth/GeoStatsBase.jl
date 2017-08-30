a769420861908bf227b373da5949ca09b72cc7dd
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-08-29 23:21:05 -0700

    Add tests for GeoDataFrame

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using GeoStatsBase
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+# list of maintainers
+maintainers = ["juliohm"]
+
+# environment settings
+istravis = "TRAVIS" ∈ keys(ENV)
+ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
+datadir = joinpath(@__DIR__,"data")
+
+# load some data
+fname = joinpath(datadir,"data2D.tsv")
+data2D = readtable(fname, coordnames=[:x,:y])
+fname = joinpath(datadir,"data3D.tsv")
+data3D = readtable(fname)
+
+# list of tests
+testfiles = [
+  "geodataframe.jl"
+]
+
+# run
+for testfile in testfiles
+  include(testfile)
+end
