3df241c810678ebdbb563d9bc7e00e61655d200c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-04-02 13:48:10 -0700

    Add default solve method for simulation problems

--- src/solvers.jl
+++ src/solvers.jl
@@ -32,6 +32,36 @@ Solve the `problem` with the `solver`.
 solve(::AbstractProblem, ::AbstractSolver) = error("not implemented")
 
 """
+    solve(problem, solver)
+
+Solve the simulation `problem` with the simulation `solver`.
+
+### Notes
+
+Default implementation calls `solve_single` in parallel.
+"""
+function solve(problem::SimulationProblem, solver::AbstractSimulationSolver)
+  # sanity checks
+  @assert keys(solver.params) ⊆ keys(variables(problem)) "invalid variable names in solver parameters"
+
+  realizations = []
+  for (var,V) in variables(problem)
+    if nworkers() > 1
+      # generate realizations in parallel
+      λ = _ -> solve_single(problem, var, solver)
+      varreals = pmap(λ, 1:nreals(problem))
+    else
+      # fallback to serial execution
+      varreals = [solve_single(problem, var, solver) for i=1:nreals(problem)]
+    end
+
+    push!(realizations, var => varreals)
+  end
+
+  SimulationSolution(domain(problem), Dict(realizations))
+end
+
+"""
     solve_single(problem, var, solver)
 
 Solve a single realization of `var` in the simulation `problem`
