ec62790bc34e6ea7c38c395c63d7045cc63f24cd
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-01-13 10:19:31 -0200

    Generalize CopyMapper to accept vector of target indices in domain

--- src/mappers/copy_mapper.jl
+++ src/mappers/copy_mapper.jl
@@ -8,7 +8,11 @@
 A mapping strategy in which data points are copied directly to the
 domain at the same location.
 """
-struct CopyMapper <: AbstractMapper end
+struct CopyMapper <: AbstractMapper
+  inds::Union{Vector{Int},Nothing}
+end
+
+CopyMapper() = CopyMapper(nothing)
 
 function Base.map(spatialdata::S, domain::D, targetvars::Vector{Symbol},
                   mapper::CopyMapper) where {S<:AbstractSpatialData,D<:AbstractDomain}
@@ -17,11 +21,16 @@ function Base.map(spatialdata::S, domain::D, targetvars::Vector{Symbol},
   # dictionary with mappings
   mappings = Dict(var => Dict{Int,Int}() for var in targetvars)
 
+  # indices in domain where to copy the data
+  inds = mapper.inds ≠ nothing ? inds : 1:npoints(spatialdata)
+
+  @assert length(inds) == npoints(spatialdata) "invalid indices in copy mapper"
+
   for location in 1:npoints(spatialdata)
     # save pair if there is data for variable
     for var in targetvars
       if isvalid(spatialdata, location, var)
-        push!(mappings[var], location => location)
+        push!(mappings[var], inds[location] => location)
       end
     end
   end
