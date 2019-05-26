dfb3a96d99ba7dbf764dbcbef21d7f0100b05881
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-05-26 17:38:08 -0300

    Add show methods for domain types

--- src/domains.jl
+++ src/domains.jl
@@ -88,3 +88,11 @@ Return a view of `domain` with all points in `locations`.
 """
 Base.view(domain::AbstractDomain,
           locations::AbstractVector{Int}) = DomainView(domain, locations)
+
+# ------------
+# IO methods
+# ------------
+function Base.show(io::IO, domain::AbstractDomain{T,N}) where {N,T<:Real}
+  npts = npoints(domain)
+  print(io, "$npts SpatialDomain{$T,$N}")
+end
--- src/domainview.jl
+++ src/domainview.jl
@@ -22,3 +22,18 @@ npoints(view::DomainView) = length(view.locations)
 
 coordinates!(buff::AbstractVector, view::DomainView, location::Int) =
   coordinates!(buff, view.domain, view.locations[location])
+
+# ------------
+# IO methods
+# ------------
+function Base.show(io::IO, view::DomainView{T,N,D,I}) where {T<:Real,N,
+                                                             D<:AbstractDomain{T,N},
+                                                             I<:AbstractVector{Int}}
+  npts = npoints(view)
+  print(io, "$npts DomainView{$T,$N}")
+end
+
+function Base.show(io::IO, ::MIME"text/plain", view::DomainView)
+  println(io, view)
+  Base.print_array(io, coordinates(view))
+end
