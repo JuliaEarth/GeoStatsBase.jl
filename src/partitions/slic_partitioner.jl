20074a295a3c4bdfea37328351fdc44a9ea224ac
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-07-03 08:36:08 -0300

    Add error update in SLICPartitioner

--- src/partitions/slic_partitioner.jl
+++ src/partitions/slic_partitioner.jl
@@ -3,62 +3,96 @@
 # ------------------------------------------------------------------
 
 """
-    SLICPartitioner(k, m, [features])
+    SLICPartitioner(k, m; tol=1e-4, maxiter=10, vars=nothing)
 
 A method for partitioning spatial data into approximately `k`
 clusters using Simple Linear Iterative Clustering (SLIC).
 The method produces clusters of samples that are spatially
 connected based on a distance `dₛ` and that, at the same
-time, are similar in terms of `features` with distance `dₓ`.
+time, are similar in terms of `vars` with distance `dᵥ`.
 The tradeoff is controlled with a weight parameter `m` in
-an additive model `d = √(dₓ² + m²dₛ²)`.
+an additive model `d = √(dᵥ² + m²dₛ²)`.
+
+## Parameters
+
+* `k`       - Approximate number of clusters
+* `m`       - Normalization constant in feature space
+* `tol`     - Tolerance of clustering algorithm (default to `1e-4`)
+* `maxiter` - Maximum number of iterations (default to `10`)
+* `vars`    - Variables (or features) to consider (default to all)
+
+## References
+
+* Achanta et al. 2011. SLIC superpixels compared to
+  state-of-the-art superpixel methods.
 """
 struct SLICPartitioner <: AbstractPartitioner
+  # hyperparameters
   k::Int
   m::Float64
-  features::Union{Vector{Symbol},Nothing}
+
+  # performance parameters
+  tol::Float64
+  maxiter::Int
+
+  # spatial variables to consider
+  vars::Union{Vector{Symbol},Nothing}
 end
 
-SLICPartitioner(k::Int, m::Real) = SLICPartitioner(k, m, nothing)
+SLICPartitioner(k::Int, m::Real; tol=1e-4, maxiter=10, vars=nothing) =
+  SLICPartitioner(k, m, tol, maxiter, nothing)
 
-function partition(spatialdata::AbstractData, partitioner::SLICPartitioner)
-  V = boundvolume(spatialdata)
-  n = npoints(spatialdata)
-  N = ndims(spatialdata)
-  k = partitioner.k
-  s = (V/k) ^ (1/N)
+function partition(sdata::AbstractData, partitioner::SLICPartitioner)
+  # initial spacing of clusters
+  s = slic_spacing(sdata, partitioner)
 
   # initialize cluster centers
-  c = slic_initialization(spatialdata, s)
+  c = slic_initialization(sdata, s)
 
   # ball neighborhood for local search
-  neigh = BallNeighborhood(spatialdata, s)
+  neigh = BallNeighborhood(sdata, s)
 
   # pre-allocate memory for label and distance
-  l = fill(0, n); d = fill(Inf, n)
+  l = fill(0, npoints(sdata))
+  d = fill(Inf, npoints(sdata))
+
+  # performance parameters
+  tol     = partitioner.tol
+  maxiter = partitioner.maxiter
 
   # k-means algorithm
   err, iter = Inf, 0
-  while iter < 10 # TODO: add err > threshold condition
-    slic_assignment!(spatialdata, neigh, c, d, l)
-    slic_update!(spatialdata, c, l)
-    # TODO: compute error
+  while err > tol && iter < maxiter
+    o = copy(c)
+
+    slic_assignment!(sdata, neigh, c, d, l)
+    slic_update!(sdata, c, l)
+
+    err = norm(c - o) / norm(o)
     iter += 1
   end
 
   subsets = [findall(isequal(k), l) for k in 1:length(c)]
 
-  SpatialPartition(spatialdata, subsets)
+  SpatialPartition(sdata, subsets)
 end
 
-function slic_initialization(spatialdata::AbstractData, s::Real)
+function slic_spacing(sdata::AbstractData,
+                      partitioner::SLICPartitioner)
+  V = boundvolume(sdata)
+  d = ndims(sdata)
+  k = partitioner.k
+  (V/k) ^ (1/d)
+end
+
+function slic_initialization(sdata::AbstractData, s::Real)
   # efficient neighbor search
-  searcher = NearestNeighborSearcher(spatialdata, 1)
+  searcher = NearestNeighborSearcher(sdata, 1)
 
   # cluster centers
   clusters = Vector{Int}()
   neighbor = Vector{Int}(undef, 1)
-  ranges = [lo:s:up for (lo,up) in bounds(spatialdata)]
+  ranges = [lo:s:up for (lo,up) in bounds(sdata)]
   for x in Iterators.product(ranges...)
     search!(neighbor, SVector(x), searcher)
     push!(clusters, neighbor[1])
@@ -67,16 +101,22 @@ function slic_initialization(spatialdata::AbstractData, s::Real)
   unique(clusters)
 end
 
-function slic_assignment!(spatialdata::AbstractData,
+function slic_assignment!(sdata::AbstractData,
                           neigh::BallNeighborhood,
                           c::AbstractVector{Int},
                           d::AbstractVector{Float64},
                           l::AbstractVector{Int})
   for (k, cₖ) in enumerate(c)
     inds = neigh(cₖ)
-    X  = coordinates(spatialdata, inds)
-    xₖ = coordinates(spatialdata, [cₖ])
+
+    # spatial distance
+    X  = coordinates(sdata, inds)
+    xₖ = coordinates(sdata, [cₖ])
     dₛ = pairwise(Euclidean(), X, xₖ, dims=2)
+
+    # feature distance
+    # TODO:
+
     @inbounds for (i, ind) in enumerate(inds)
       if dₛ[i] < d[ind]
         d[ind] = dₛ[i]
@@ -86,12 +126,12 @@ function slic_assignment!(spatialdata::AbstractData,
   end
 end
 
-function slic_update!(spatialdata::AbstractData,
+function slic_update!(sdata::AbstractData,
                       c::AbstractVector{Int},
                       l::AbstractVector{Int})
   for k in 1:length(c)
     inds = findall(isequal(k), l)
-    X  = coordinates(spatialdata, inds)
+    X  = coordinates(sdata, inds)
     μ  = mean(X, dims=2)
     dₛ = pairwise(Euclidean(), X, μ, dims=2)
     @inbounds c[k] = argmin(vec(dₛ))
