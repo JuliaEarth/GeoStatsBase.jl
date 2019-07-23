83df49816f16700e6a8f8c3606394a6ac789484f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-07-22 21:15:30 -0300

    Add more tests for problems

--- test/problems.jl
+++ test/problems.jl
@@ -5,27 +5,36 @@
   grid3D = RegularGrid{Float64}(100,100,100)
 
   @testset "Estimation" begin
-    problem = EstimationProblem(data3D, grid3D, :value)
-    @test data(problem) == data3D
-    @test domain(problem) == grid3D
-    @test variables(problem) == Dict(:value => Float64)
+    # test basic problem interface
+    problem3D = EstimationProblem(data3D, grid3D, :value)
+    @test data(problem3D) == data3D
+    @test domain(problem3D) == grid3D
+    @test variables(problem3D) == Dict(:value => Float64)
+
+    # dimension mismatch
     @test_throws AssertionError EstimationProblem(data3D, grid2D, :value)
 
     # show methods
-    grid2D = RegularGrid{Float64}(100,200)
-    problem = EstimationProblem(data2D, grid2D, :value)
-    @test sprint(show, problem) == "2D EstimationProblem"
-    @test sprint(show, MIME"text/plain"(), problem) == "2D EstimationProblem\n  data:      3×3 GeoDataFrame (x and y)\n  domain:    100×200 RegularGrid{Float64,2}\n  variables: value (Float64)"
+    problem2D = EstimationProblem(data2D, grid2D, :value)
+    @test sprint(show, problem2D) == "2D EstimationProblem"
+    @test sprint(show, MIME"text/plain"(), problem2D) == "2D EstimationProblem\n  data:      3×3 GeoDataFrame (x and y)\n  domain:    100×100 RegularGrid{Float64,2}\n  variables: value (Float64)"
+
+    if visualtests
+      gr(size=(800,800))
+      @plottest plot(problem2D,ms=2) joinpath(datadir,"EstimationProblem2D.png") !istravis
+    end
   end
 
   @testset "Simulation" begin
     # test basic problem interface
-    problem = SimulationProblem(data3D, grid3D, :value, 100)
-    @test data(problem) == data3D
-    @test domain(problem) == grid3D
-    @test variables(problem) == Dict(:value => Float64)
-    @test hasdata(problem)
-    @test nreals(problem) == 100
+    problem3D = SimulationProblem(data3D, grid3D, :value, 100)
+    @test data(problem3D) == data3D
+    @test domain(problem3D) == grid3D
+    @test variables(problem3D) == Dict(:value => Float64)
+    @test hasdata(problem3D)
+    @test nreals(problem3D) == 100
+
+    # dimension mismatch
     @test_throws AssertionError SimulationProblem(data3D, grid2D, :value, 100)
 
     # specify type of variable explicitly
@@ -49,9 +58,17 @@
     @test_throws MethodError SimulationProblem(grid3D, :value, 100)
 
     # show methods
-    grid2D = RegularGrid{Float64}(100,200)
-    problem = SimulationProblem(data2D, grid2D, :value, 100)
-    @test sprint(show, problem) == "2D SimulationProblem (conditional)"
-    @test sprint(show, MIME"text/plain"(), problem) == "2D SimulationProblem (conditional)\n  data:      3×3 GeoDataFrame (x and y)\n  domain:    100×200 RegularGrid{Float64,2}\n  variables: value (Float64)\n  N° reals:  100"
+    problem2D = SimulationProblem(data2D, grid2D, :value, 100)
+    @test sprint(show, problem2D) == "2D SimulationProblem (conditional)"
+    @test sprint(show, MIME"text/plain"(), problem2D) == "2D SimulationProblem (conditional)\n  data:      3×3 GeoDataFrame (x and y)\n  domain:    100×100 RegularGrid{Float64,2}\n  variables: value (Float64)\n  N° reals:  100"
+
+    if visualtests
+      gr(size=(800,800))
+      @plottest plot(problem2D,ms=2) joinpath(datadir,"SimulationProblem2D.png") !istravis
+    end
+  end
+
+  @testset "Learning" begin
+    # TODO:
   end
 end
