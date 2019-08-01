fa76bdbabdc3936fb621471fb618f59ba7830f87
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-08-01 17:09:59 -0300

    Update histplot

--- src/plotrecipes/histplot.jl
+++ src/plotrecipes/histplot.jl
@@ -4,38 +4,44 @@
 
 @userplot HistPlot
 
-@recipe function f(hp::HistPlot; showcdf=true)
+@recipe function f(hp::HistPlot; showcdf=false)
   # retrieve inputs
   sdata = hp.args[1]
   vars  = length(hp.args) == 2 ? hp.args[2] : keys(variables(sdata))
 
   layout --> length(vars)
   legend --> false
+  color --> :black
 
   for (i, var) in enumerate(vars)
-    xguide --> var
     h = normalize(histogram(sdata, var))
+    q = quantile(sdata, var, [0.25,0.50,0.75])
     x = midpoints(h.edges[1])
     y = h.weights
+    s = x[2] - x[1]
+
     @series begin
       subplot := i
-      seriestype --> :bar
-      color --> :black
-      x, y
-    end
-    if showcdf
-      @series begin
-        subplot := i
-        seriestype --> :step
-        color --> :red
-        linewidth --> 2
+      seriestype --> :step
+      if showcdf
         ysum = cumsum(y)
         ycdf = ysum ./ ysum[end]
-        step = x[2] - x[1]
-        xe = vcat(x[1]-step, x, x[end]+step)
+        xe = vcat(x[1]-s, x, x[end]+s)
         ye = vcat(0, ycdf, 1)
-        xe, ye
+      else
+        xe = vcat(x[1]-s, x, x[end]+s)
+        ye = vcat(0, y, 0)
       end
+      xe, ye
+    end
+
+    @series begin
+      subplot := i
+      primary := false
+      seriestype --> :vline
+      linestyle --> :dash
+      xguide --> var
+      q
     end
   end
 end
