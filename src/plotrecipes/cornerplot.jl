aa094a262880464a3091f90843a57a716ccd3563
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-08-02 15:17:04 -0300

    Add size option to cornerplot

--- src/plotrecipes/cornerplot.jl
+++ src/plotrecipes/cornerplot.jl
@@ -4,7 +4,8 @@
 
 function cornerplot(sdata::AbstractData, vars=nothing;
                     quantiles=[0.25,0.50,0.75], cdf=false,
-                    bandwidthx=100, bandwidthy=100)
+                    bandwidthx=100, bandwidthy=100,
+                    size=(800,800))
   # variables in alphabetical order
   vars  = vars ≠ nothing ? vars : collect(keys(variables(sdata)))
   sort!(vars); n = length(vars)
@@ -21,5 +22,5 @@ function cornerplot(sdata::AbstractData, vars=nothing;
     end
   end
 
-  RecipesBase.plot(plts..., layout=(n,n))
+  RecipesBase.plot(plts..., layout=(n,n), size=size)
 end
