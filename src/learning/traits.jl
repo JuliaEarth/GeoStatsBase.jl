14cb249b0b9f50d7d6f5ce276f4bb2ee64d09b88
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-08-04 14:37:06 -0300

    Add iscompatible trait

--- src/learning/traits.jl
+++ src/learning/traits.jl
@@ -10,3 +10,15 @@ Check whether or not `task` is supervised.
 issupervised(task::AbstractLearningTask) = false
 issupervised(task::SupervisedLearningTask) = true
 issupervised(task::UnsupervisedLearningTask) = false
+
+"""
+    iscompatible(model, task)
+
+Check whether or not `model` can be used for
+learning `task`.
+"""
+iscompatible(model::MLJBase.Model, task::AbstractLearningTask) = false
+iscompatible(model::MLJBase.Model, task::RegressionTask) =
+  MLJBase.target_scitype_union(model) == MLJBase.Continuous
+iscompatible(model::MLJBase.Model, task::ClassificationTask) =
+  MLJBase.target_scitype_union(model) == MLJBase.Count
