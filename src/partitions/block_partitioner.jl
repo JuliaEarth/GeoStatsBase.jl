a7315677d79e1befb4920251ab30a09e7d76b24c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-08-12 22:08:15 -0300

    Refactor BlockPartitioner with new boundbox interface

--- src/partitions/block_partitioner.jl
+++ src/partitions/block_partitioner.jl
@@ -14,21 +14,23 @@ end
 function partition(object::AbstractSpatialObject{T,N},
                    partitioner::BlockPartitioner) where {N,T}
   side = partitioner.side
+  bbox = boundbox(object)
 
-  objbounds  = bounds(object)
-  lowerleft  = first.(objbounds)
-  upperright = last.(objbounds)
+  @assert side ≤ minimum(sides(bbox)) "block side is too large"
 
-  @assert minimum(upperright .- lowerleft) ≥ side "block side is too large"
+  # bounding box properties
+  ce = center(bbox)
+  lo = lowerleft(bbox)
+  up = upperright(bbox)
 
-  center = @. (lowerleft + upperright) / 2
-  Δleft  = @. ceil(Int, (center  - lowerleft) / side)
-  Δright = @. ceil(Int, (upperright - center) / side)
+  # find number of blocks to left and right
+  nleft  = @. ceil(Int, (ce - lo) / side)
+  nright = @. ceil(Int, (up - ce) / side)
 
-  origin  = @. center - Δleft*side
-  nblocks = @. Δleft + Δright
+  origin  = @. ce - nleft * side
+  nblocks = @. nleft + nright
 
-  subsets = [Int[] for i in 1:prod(nblocks)]
+  subsets = [Vector{Int}() for i in 1:prod(nblocks)]
 
   coords  = MVector{N,T}(undef)
   linear = LinearIndices(Dims(nblocks))
@@ -36,11 +38,13 @@ function partition(object::AbstractSpatialObject{T,N},
     coordinates!(coords, object, j)
 
     # find block coordinates
-    c = floor.(Int, (Tuple(coords) .- origin) ./ side) .+ 1
-    bcoords = ntuple(i->@inbounds(return clamp(c[i], 1, nblocks[i])), N)
+    c = @. floor(Int, (coords - origin) / side) + 1
+    @inbounds for i in 1:N
+      c[i] = clamp(c[i], 1, nblocks[i])
+    end
 
     # block index
-    i = linear[bcoords...]
+    i = linear[c...]
 
     append!(subsets[i], j)
   end
