9a21069c6f0a41917e8bc0f918fc6cb1b01f9265
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-08-13 21:49:50 -0300

    Rename filenames in tasks directory

--- src/learning/tasks.jl
+++ src/learning/tasks.jl
@@ -41,6 +41,6 @@ abstract type UnsupervisedLearningTask <: AbstractLearningTask end
 #------------------
 # IMPLEMENTATIONS
 #------------------
-include("tasks/regression_task.jl")
-include("tasks/classification_task.jl")
-include("tasks/clustering_task.jl")
+include("tasks/regression.jl")
+include("tasks/classification.jl")
+include("tasks/clustering.jl")
