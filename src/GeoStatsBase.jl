5eab6574d52cfb98385cb4d193d150ca0b62eb0b
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-08-24 14:57:10 -0300

    Update test suite

--- src/GeoStatsBase.jl
+++ src/GeoStatsBase.jl
@@ -117,7 +117,7 @@ export
   domain,
   sourcedata,
   targetdata,
-  tasks,
+  task,
   mapper,
   variables,
   coordinates,
--- test/problems.jl
+++ test/problems.jl
@@ -84,8 +84,8 @@
     Random.seed!(123)
     sdata = PointSetData(Dict(:x=>rand(10), :y=>rand(10), :z=>rand(10)), 10rand(2,10))
     tdata = RegularGridData{Float64}(Dict(:x=>rand(10,10)))
-    rtask = RegressionTask((:x,), :y)
-    ctask = ClusteringTask((:x,))
+    rtask = RegressionTask(:x, :y)
+    ctask = ClusteringTask(:x, :c)
 
     # test basic problem interface
     problem = LearningProblem(sdata, tdata, rtask)
@@ -100,7 +100,7 @@
     # show methods
     problem = LearningProblem(sdata, tdata, ctask)
     @test sprint(show, problem) == "2D LearningProblem"
-    @test sprint(show, MIME"text/plain"(), problem) == "2D LearningProblem\n  source\n    └─data: 10 PointSetData{Float64,2}\n  target\n    └─data: 10×10 RegularGridData{Float64,2}\n  tasks: Clustering (x)\n"
+    @test sprint(show, MIME"text/plain"(), problem) == "2D LearningProblem\n  source\n    └─data: 10 PointSetData{Float64,2}\n  target\n    └─data: 10×10 RegularGridData{Float64,2}\n  task: Clustering x → c\n"
 
     if visualtests
       gr(size=(800,800))
