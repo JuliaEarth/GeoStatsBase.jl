4906aa7e6c7f366c81c12d7f3351715b8ef0f0ab
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-08-28 09:11:08 -0300

    Add split utility function

--- src/utils.jl
+++ src/utils.jl
@@ -16,3 +16,19 @@ This function returns a [`GeoDataFrame`](@ref) object.
 """
 readgeotable(args...; coordnames=[:x,:y,:z], kwargs...) =
   GeoDataFrame(read(args...; kwargs...), coordnames)
+
+"""
+      split(object, fraction, [direction])
+
+Split spatial `object` into two parts where the first
+part has a `fraction` of the total volume. The split
+is performed along a `direction`. The default direction
+is aligned with the first spatial dimension of the object.
+"""
+function Base.split(object::AbstractSpatialObject{T,N},
+                    fraction::Real, direction=nothing) where {N,T}
+  normal = direction ≠ nothing ? direction :
+                                 ntuple(i -> i == 1 ? one(T) : zero(T), N)
+  partitioner = BisectFractionPartitioner(normal, fraction)
+  partition(object, partitioner)
+end
