943b6b0c8045b42a01ae09b8f66a0afaabc0d755
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-10-15 21:09:17 -0300

    Add more tests for learning tasks

--- test/learning.jl
+++ test/learning.jl
@@ -5,16 +5,31 @@
     @test outputvars(t) == (:y,)
     @test features(t) == (:x,)
     @test label(t) == :y
+    @test sprint(show, t) == "Regression x → y"
+    t = RegressionTask([:x,:y], :z)
+    @test inputvars(t) == (:x,:y)
+    @test features(t) == (:x,:y)
+    @test sprint(show, t) == "Regression (x, y) → z"
 
     t = ClassificationTask(:x,:y)
     @test inputvars(t) == (:x,)
     @test outputvars(t) == (:y,)
     @test features(t) == (:x,)
     @test label(t) == :y
+    @test sprint(show, t) == "Classification x → y"
+    t = ClassificationTask([:x,:y], :z)
+    @test inputvars(t) == (:x,:y)
+    @test features(t) == (:x,:y)
+    @test sprint(show, t) == "Classification (x, y) → z"
 
     t = ClusteringTask(:x,:y)
     @test inputvars(t) == (:x,)
     @test outputvars(t) == (:y,)
     @test features(t) == (:x,)
+    @test sprint(show, t) == "Clustering x → y"
+    t = ClusteringTask([:x,:y], :z)
+    @test inputvars(t) == (:x,:y)
+    @test features(t) == (:x,:y)
+    @test sprint(show, t) == "Clustering (x, y) → z"
   end
 end
