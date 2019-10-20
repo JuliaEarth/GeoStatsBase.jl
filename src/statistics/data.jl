682114e148ba2ffb5a155ffa6669251b15bbed88
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-10-20 20:36:31 -0300

    Add docstring for mean/var/quantile of spatial data

--- src/statistics/data.jl
+++ src/statistics/data.jl
@@ -4,6 +4,11 @@
 
 const RealOrVec = Union{Real,AbstractVector}
 
+"""
+    mean(spatialdata)
+
+Spatial mean of `spatialdata`.
+"""
 mean(d::AbstractData, v::Symbol, w::AbstractWeighter) = mean(vec(d[v]), weight(d, w))
 mean(d::AbstractData, v::Symbol, blockside::Real) = mean(d, v, BlockWeighter(blockside))
 mean(d::AbstractData, v::Symbol) = mean(d, v, median_heuristic(d))
@@ -11,6 +16,11 @@ mean(d::AbstractData, w::AbstractWeighter) = Dict(v => mean(d, v, w) for (v,V) i
 mean(d::AbstractData, blockside::Real) = mean(d, BlockWeighter(blockside))
 mean(d::AbstractData) = mean(d, median_heuristic(d))
 
+"""
+    var(spatialdata)
+
+Spatial variance of `spatialdata`.
+"""
 var(d::AbstractData, v::Symbol, w::AbstractWeighter) = var(vec(d[v]), weight(d, w), mean=mean(d, v, w), corrected=false)
 var(d::AbstractData, v::Symbol, blockside::Real) = var(d, v, BlockWeighter(blockside))
 var(d::AbstractData, v::Symbol) = var(d, v, median_heuristic(d))
@@ -18,6 +28,11 @@ var(d::AbstractData, w::AbstractWeighter) = Dict(v => var(d, v, w) for (v,V) in 
 var(d::AbstractData, blockside::Real) = var(d, BlockWeighter(blockside))
 var(d::AbstractData) = var(d, median_heuristic(d))
 
+"""
+    quantile(spatialdata, p)
+
+Spatial quantile of `spatialdata`.
+"""
 quantile(d::AbstractData, v::Symbol, p::T, w::AbstractWeighter) where {T<:RealOrVec} = quantile(vec(d[v]), weight(d, w), p)
 quantile(d::AbstractData, v::Symbol, p::T, blockside::Real) where {T<:RealOrVec} = quantile(d, v, p, BlockWeighter(blockside))
 quantile(d::AbstractData, v::Symbol, p::T) where {T<:RealOrVec} = quantile(d, v, p, median_heuristic(d))
@@ -25,6 +40,11 @@ quantile(d::AbstractData, p::T, w::AbstractWeighter) where {T<:RealOrVec} = Dict
 quantile(d::AbstractData, p::T, blockside::Real) where {T<:RealOrVec} = quantile(d, p, BlockWeighter(blockside))
 quantile(d::AbstractData, p::T) where {T<:RealOrVec} = quantile(d, p, median_heuristic(d))
 
+"""
+    histogram(spatialdata)
+
+Spatial histogram of `spatialdata`.
+"""
 histogram(d::AbstractData, v::Symbol, w::AbstractWeighter) = fit(Histogram, vec(d[v]), weight(d, w))
 histogram(d::AbstractData, v::Symbol, blockside::Real) = histogram(d, v, BlockWeighter(blockside))
 histogram(d::AbstractData, v::Symbol) = histogram(d, v, median_heuristic(d))
