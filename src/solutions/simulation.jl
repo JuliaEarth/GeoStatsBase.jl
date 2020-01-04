e84725d7837c0f5342346691bdcc06f77ff5979d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-01-04 15:26:00 -0300

    Improve SimulationSolution API

--- src/solutions/simulation.jl
+++ src/solutions/simulation.jl
@@ -10,6 +10,13 @@ A solution to a spatial simulation problem.
 struct SimulationSolution{D<:AbstractDomain}
   domain::D
   realizations::Dict{Symbol,Vector{<:AbstractVector}}
+  nreals::Int
+
+  function SimulationSolution{D}(domain, realizations) where {D}
+    n = [length(r) for (var, r) in realizations]
+    @assert length(unique(n)) == 1 "number of realizations must be unique"
+    new(domain, realizations, n[1])
+  end
 end
 
 SimulationSolution(domain, realizations) =
@@ -30,6 +37,58 @@ function Base.getindex(solution::SimulationSolution{<:RegularGrid}, var::Symbol)
   [reshape(real, sz) for real in solution.realizations[var]]
 end
 
+#---------------
+# ITERATOR API
+#---------------
+
+"""
+    iterate(solution, state=1)
+
+Iterate over realizations in simulation `solution`.
+"""
+Base.iterate(solution::SimulationSolution, state=1) =
+  state > solution.nreals ? nothing : (solution[state], state + 1)
+
+"""
+    length(solution)
+
+Return the number of realizations in simulation `solution`.
+"""
+Base.length(solution::SimulationSolution) = solution.nreals
+
+#----------------
+# INDEXABLE API
+#----------------
+
+"""
+    getindex(solution, ind)
+
+Return the `ind`-th realization of simulation `solution`.
+"""
+function Base.getindex(solution::SimulationSolution, ind::Int)
+  sdomain = solution.domain
+  sreals  = solution.realizations
+  idata   = Dict(var => reals[ind] for (var, reals) in sreals)
+  georeference(idata, sdomain)
+end
+
+Base.getindex(solution::SimulationSolution, inds::AbstractVector{Int}) =
+  [getindex(solution, ind) for ind in inds]
+
+"""
+    firstindex(solution)
+
+Return the first index of simulation `solution`.
+"""
+Base.firstindex(solution::SimulationSolution) = 1
+
+"""
+    lastindex(solution)
+
+Return the last index of simulation `solution`.
+"""
+Base.lastindex(solution::SimulationSolution) = length(solution)
+
 # ------------
 # IO methods
 # ------------
@@ -41,5 +100,6 @@ end
 function Base.show(io::IO, ::MIME"text/plain", solution::SimulationSolution)
   println(io, solution)
   println(io, "  domain: ", solution.domain)
-  print(  io, "  variables: ", join(keys(solution.realizations), ", ", " and "))
+  println(io, "  variables: ", join(keys(solution.realizations), ", ", " and "))
+  print(  io, "  N° reals:  ", solution.nreals)
 end
