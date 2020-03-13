9c058888ac4a730b9aa536be33006b4c701fbd8a
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-03-12 21:17:03 -0300

    Add lambda option to DensityRatioValidation

--- src/errors/density_ratio_validation.jl
+++ src/errors/density_ratio_validation.jl
@@ -12,16 +12,24 @@ ratio estimation, and then used in `k`-fold weighted cross-validation.
 
 * `estimator` - Density ratio estimator (default to `LSIF()`)
 * `optlib`    - Optimization library (default to `default_optlib(estimator)`)
+* `lambda`    - Power of density ratios (default to `1.0`)
 """
-struct DensityRatioValidation <: AbstractErrorEstimator
+struct DensityRatioValidation{T,E,O} <: AbstractErrorEstimator
   k::Int
-  dre
-  optlib
+  lambda::T
+  dre::E
+  optlib::O
 end
 
-DensityRatioValidation(k::Int; estimator=LSIF(),
-                       optlib=default_optlib(estimator)) =
-  DensityRatioValidation(k, estimator, optlib)
+function DensityRatioValidation(k::Int; lambda=1.0, estimator=LSIF(),
+                                optlib=default_optlib(estimator))
+  @assert k > 0 "number of folds must be positive"
+  @assert 0 ≤ lambda ≤ 1 "lambda must lie in [0,1]"
+  T = typeof(lambda)
+  E = typeof(estimator)
+  O = typeof(optlib)
+  DensityRatioValidation{T,E,O}(k, lambda, estimator, optlib)
+end
 
 function Base.error(solver::AbstractLearningSolver,
                     problem::LearningProblem,
@@ -32,6 +40,8 @@ function Base.error(solver::AbstractLearningSolver,
   weighter = DensityRatioWeighter(DΩt, variables=vars,
                                   estimator=eestimator.dre,
                                   optlib=eestimator.optlib)
-  wcv = WeightedCrossValidation(weighter, eestimator.k, shuffle=true)
+  wcv = WeightedCrossValidation(weighter,
+                                eestimator.k, shuffle=true,
+                                lambda=eestimator.lambda)
   error(solver, problem, wcv)
 end
