6bcb36a66cfa160563e11db884dfba678aff47e2
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-06-15 22:03:56 -0300

    Add tests for plot recipes

--- test/plotrecipes.jl
+++ test/plotrecipes.jl
@@ -1,13 +1,24 @@
 @testset "Protting" begin
+  Random.seed!(123)
+  z₁ = randn(100,100)
+  z₂ = z₁ + randn(100,100)
+  d = RegularGridData{Float64}(OrderedDict(:z₁=>z₁,:z₂=>z₂))
+
   @testset "DistPlot1D" begin
-    # TODO
+    if visualtests
+      @plottest distplot1d(d,:z₁) joinpath(datadir,"distplot1D.png") !istravis
+    end
   end
 
   @testset "DistPlot2D" begin
-    # TODO
+    if visualtests
+      @plottest distplot2d(d,:z₁,:z₂) joinpath(datadir,"distplot2D.png") !istravis
+    end
   end
 
   @testset "CornerPlot" begin
-    # TODO
+    if visualtests
+      @plottest cornerplot(d) joinpath(datadir,"cornerplot.png") !istravis
+    end
   end
 end
