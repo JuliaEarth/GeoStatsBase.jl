abf2ff737551ea1e676c298ea0589caedbac8bc5
Exequiel <exequiel.sepulvedaescobedo@adelaide.edu.au>
2020-08-06 09:25:23 +0930

    Fixing formatting. Spatial indexing is done for samples coordinates

--- src/neighborsearch/kball_searcher.jl
+++ src/neighborsearch/kball_searcher.jl
@@ -12,55 +12,43 @@ Used when doing estimation combining k-nearest and radius.
 It searches first for the k-nearest by using the internal kdtree and then
 limiting the results within the `BallNeighborhood`
 """
-struct KBallSearcher{O,K,T,M} <: AbstractBoundedNeighborSearcher
-
+struct KBallSearcher{O,B,K} <: AbstractBoundedNeighborSearcher
   # input fields
   object::O
   k::Int
-  ball::BallNeighborhood{T,M}
+  ball::B
   locations::Vector{Int}
 
   # state fields
   kdtree::K
-
 end
 
 
-function KBallSearcher(object::O, k::Int, ball::BallNeighborhood{T,M}; locations=1:npoints(object)) where {O,T,M}
+function KBallSearcher(object::O, k::Int, ball::B; locations=1:npoints(object)) where {O,B}
   @assert 1 ≤ k ≤ length(locations) "number of neighbors must be smaller than number of locations"
   @assert locations ⊆ 1:npoints(object) "invalid locations for spatial object"
-  kdtree = KDTree(coordinates(object, locations), ball.metric)
-  KBallSearcher{O,typeof(kdtree),T,M}(object, k, ball, locations, kdtree)
+  kdtree = KDTree(coordinates(object, locations), metric(ball))
+  KBallSearcher{O,B,typeof(kdtree)}(object, k, ball, locations, kdtree)
 end
 
-maxneighbors(searcher::KBallSearcher) = searcher.nmax
+maxneighbors(searcher::KBallSearcher) = searcher.k
 
 function search!(neighbors::AbstractVector{Int}, xₒ::AbstractVector,
                  searcher::KBallSearcher; mask=nothing)
-
-  k      = searcher.k
-  ball   = searcher.ball
-  radius = ball.radius
+  k    = searcher.k
+  ball = searcher.ball
+  r    = radius(ball)
 
   inds, dists = knn(searcher.kdtree, xₒ, k, true)
-  locs = view(searcher.locations, inds) #translate from relative indices (inds) to pdomain indices
+  locs = view(searcher.locations, inds)
 
-  if mask ≠ nothing
-    nneigh = 0
-    @inbounds for i in 1:length(inds)
-      #if mask is true, the locations is already
-      if mask[locs[i]] && (dists[i] ≤ radius)
-        nneigh += 1
-        neighbors[nneigh] = locs[i]
-      end
-    end
-  else
-    nneigh = 0
-    @inbounds for i in 1:length(inds)
-      if dists[i] ≤ radius
-        nneigh += 1
-        neighbors[nneigh] = locs[i]
-      end
+  nneigh = 0
+  @inbounds for i in 1:length(inds)
+    #if mask is true, the locations must be skipped
+    useit = (mask === nothing) || !mask[locs[i]]
+    if useit && (dists[i] ≤ r)
+      nneigh += 1
+      neighbors[nneigh] = locs[i]
     end
   end
 
--- test/neighborsearch.jl
+++ test/neighborsearch.jl
@@ -1,59 +1,53 @@
 @testset "Neighborsearch" begin
   @testset "KBallSearcher" begin
-    # KBallSearcher
-
     #create a regular grid to test
-    pdomain = RegularGrid(10,10)
+    domain = RegularGrid(10,10)
 
     #test 1: all points should be found
     k = 10
     ball = BallNeighborhood(100.0)
-    kballsearcher = KBallSearcher(pdomain, k, ball)
+    kballsearcher = KBallSearcher(domain, k, ball)
 
-    neighbors = neighbors = Vector{Int}(undef, k)
     xₒ = [5.0,5.0]
-    nneigh = search!(neighbors,xₒ,kballsearcher; mask=nothing)
+    neighbors = search(xₒ, kballsearcher)
 
-    @test nneigh == 10
+    @test length(neighbors) == 10
 
     #test 2: radius of 1.0 m 5 points should be found
     k = 10
     ball = BallNeighborhood(1.0)
-    kballsearcher = KBallSearcher(pdomain, k, ball)
+    kballsearcher = KBallSearcher(domain, k, ball)
 
-    neighbors = Vector{Int}(undef, k)
     xₒ = [5.0,5.0]
-    nneigh = search!(neighbors,xₒ,kballsearcher; mask=nothing)
-    @test nneigh == 5
+    neighbors = search(xₒ, kballsearcher)
+    @test length(neighbors) == 5
     @test neighbors[1] == 56
 
     #testing with masks
-    mask = trues(npoints(pdomain))
-    mask[56] = false #excluding location #56
-    nneigh = search!(neighbors,xₒ,kballsearcher; mask=mask)
-    @test nneigh == 4
+    mask = falses(npoints(domain))
+    mask[56] = true #excluding location #56
+    neighbors = search(xₒ, kballsearcher;mask=mask)
+    @test length(neighbors) == 4
 
     #test 3: radius of 1.0 m but far just to find 1 point
     k = 10
     ball = BallNeighborhood(1.0)
-    kballsearcher = KBallSearcher(pdomain, k, ball)
+    kballsearcher = KBallSearcher(domain, k, ball)
 
-    neighbors = neighbors = Vector{Int}(undef, k)
     xₒ = [-0.2,-0.2]
-    nneigh = search!(neighbors,xₒ,kballsearcher; mask=nothing)
+    neighbors = search(xₒ, kballsearcher)
 
-    @test nneigh == 1
+    @test length(neighbors) == 1
 
     #test 4: radius of 1.0 m but far just to find no points
     k = 10
     ball = BallNeighborhood(1.0)
-    kballsearcher = KBallSearcher(pdomain, k, ball)
+    kballsearcher = KBallSearcher(domain, k, ball)
 
-    neighbors = neighbors = Vector{Int}(undef, k)
     xₒ = [-10.0,-10.0]
-    nneigh = search!(neighbors,xₒ,kballsearcher; mask=nothing)
+    neighbors = search(xₒ, kballsearcher)
 
-    @test nneigh == 0
+    @test length(neighbors) == 0
     
   end
 end
