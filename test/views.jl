17ca447d33292e1a71ec31a8d4febb9784272532
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-08-24 14:15:39 -0300

    Add more tests for IO methods

--- test/views.jl
+++ test/views.jl
@@ -6,6 +6,9 @@
     @test nelms(v) == 10
     @test coordinates(v) == X[:,1:10]
     @test collect(v) isa PointSet
+
+    @test sprint(show, v) == "10 SpatialDomainView{Float64,2}"
+    @test sprint(show, MIME"text/plain"(), v) == "10 SpatialDomainView{Float64,2}\n 0.0  1.0  2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0\n 0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0  0.0"
   end
 
   @testset "Data" begin
@@ -16,5 +19,8 @@
     @test nelms(v) == 10
     @test coordinates(v) == X[:,1:10]
     @test collect(v) isa SpatialData
+
+    @test sprint(show, v) == "10 SpatialDataView{Float64,1}"
+    @test sprint(show, MIME"text/plain"(), v) == "10 SpatialDomainView{Float64,1}\n  variables\n    └─w (Float64)\n    └─z (Float64)" 
   end
 end
