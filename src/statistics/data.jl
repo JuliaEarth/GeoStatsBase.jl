a1a67e498477f80890556443232c3e08a415e083
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-11-13 10:20:59 -0300

    ISC == MIT

--- src/statistics/data.jl
+++ src/statistics/data.jl
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------
-# Licensed under the ISC License. See LICENSE in the project root.
+# Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
 mean(d, v::Symbol, w::AbstractWeighter) = mean(d[v], weight(d, w))
