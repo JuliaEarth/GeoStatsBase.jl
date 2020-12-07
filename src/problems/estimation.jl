194df6a9f22e2c80360c4caf035c338eaa0e7e9c
Júlio Hoffimann <julio.hoffimann@gmail.com>
2020-12-07 08:06:14 -0300

    Remove mapper from EstimationProblem

--- src/problems/estimation.jl
+++ src/problems/estimation.jl
@@ -23,40 +23,33 @@ Create an estimation problem for precipitation and CO₂:
 julia> EstimationProblem(sdata, sdomain, (:precipitation,:CO₂))
 ```
 """
-struct EstimationProblem{S,D,M} <: AbstractProblem
+struct EstimationProblem{S,D} <: AbstractProblem
   # input fields
   sdata::S
   sdomain::D
-  mapper::M
   targetvars::Dict{Symbol,DataType}
 
-  # state fields
-  mappings::Dict{Symbol,Dict{Int,Int}}
-
-  function EstimationProblem{S,D,M}(sdata, sdomain, targetvars, mapper) where {S,D,M}
+  function EstimationProblem{S,D}(sdata, sdomain, targetvars) where {S,D}
     probvnames = Tuple(keys(targetvars))
     datavnames = name.(variables(sdata))
 
     @assert !isempty(probvnames) && probvnames ⊆ datavnames "target variables must be present in spatial data"
     @assert coordtype(sdata) == coordtype(sdomain) "data and domain must have the same coordinate type"
 
-    mappings = map(sdata, sdomain, probvnames, mapper)
-
-    new(sdata, sdomain, mapper, targetvars, mappings)
+    new(sdata, sdomain, targetvars)
   end
 end
 
-function EstimationProblem(sdata::S, sdomain::D, targetvarnames::NTuple;
-                           mapper::M=NearestMapper()) where {S,D,M}
+function EstimationProblem(sdata::S, sdomain::D, targetvarnames::NTuple) where {S,D}
   # build dictionary of target variables
   vars = filter(v -> name(v) ∈ targetvarnames, variables(sdata))
   targetvars = Dict(name(var) => mactype(var) for var in vars)
 
-  EstimationProblem{S,D,M}(sdata, sdomain, targetvars, mapper)
+  EstimationProblem{S,D}(sdata, sdomain, targetvars)
 end
 
-EstimationProblem(sdata, sdomain, targetvarname::Symbol; mapper=NearestMapper()) =
-  EstimationProblem(sdata, sdomain, (targetvarname,); mapper=mapper)
+EstimationProblem(sdata, sdomain, targetvarname::Symbol) =
+  EstimationProblem(sdata, sdomain, (targetvarname,))
 
 """
     data(problem)
@@ -73,34 +66,11 @@ Return the spatial domain of the estimation `problem`.
 domain(problem::EstimationProblem) = problem.sdomain
 
 """
-    mapper(problem)
-
-Return the mapper of the estimation `problem`.
-"""
-mapper(problem::EstimationProblem) = problem.mapper
-
-"""
     variables(problem)
 
 Return the variable names of the estimation `problem` and their types.
 """
 variables(problem::EstimationProblem) = problem.targetvars
-
-"""
-    datamap(problem, targetvar)
-
-Return the mapping from domain locations to data locations for the
-`targetvar` of the `problem`.
-"""
-datamap(problem::EstimationProblem, var::Symbol) = problem.mappings[var]
-
-"""
-    datamap(problem)
-
-Return the mappings from domain locations to data locations for all
-the variables of the `problem`.
-"""
-datamap(problem::EstimationProblem) = problem.mappings
 
 # ------------
 # IO methods
