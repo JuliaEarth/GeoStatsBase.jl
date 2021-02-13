4bfa794d678cb48293fa16b3129c5f539756692f
Gaurav Wasnik <55525317+cyborg1995@users.noreply.github.com>
2021-02-13 14:42:58 +0530

    Update test/histograms.jl
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- test/histograms.jl
+++ test/histograms.jl
@@ -3,9 +3,11 @@
   z₁ = randn(10000)
   z₂ = z₁ + randn(10000)
   d = georef(DataFrame(z₁=z₁,z₂=z₂), RegularGrid(100,100))
+  h1 = EmpiricalHistogram(d, :z₁)
+  h2 = EmpiricalHistogram(d, :z₂)
 
   if visualtests
-      @test_ref_plot "data/empiricalhistogram1.png" plot(EmpiricalHistogram(d, :z₁))
-      @test_ref_plot "data/empiricalhistogram2.png" plot(EmpiricalHistogram(d, :z₂))
+      @test_ref_plot "data/empiricalhistogram1.png" plot(h1)
+      @test_ref_plot "data/empiricalhistogram2.png" plot(h2)
   end
 end
