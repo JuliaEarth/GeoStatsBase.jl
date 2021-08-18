13f0372f28aede23e2a01cd02553654e9d04b3cb
Nicholas Shea <nshea@email.arizona.edu>
2021-08-17 19:06:45 -0700

    feat: changes median_heuristic calls to mode_heuristic

--- src/statistics/data.jl
+++ src/statistics/data.jl
@@ -4,7 +4,7 @@
 
 mean(d, v::Symbol, w::WeightingMethod) = mean(d[v], weight(d, w))
 mean(d, v::Symbol, s::Number) = mean(d, v, BlockWeighting(ntuple(i->s,embeddim(d))))
-mean(d, v::Symbol) = mean(d, v, median_heuristic(d))
+mean(d, v::Symbol) = mean(d, v, mode_heuristic(d))
 mean(d, w::WeightingMethod) = Dict(v => mean(d, v, w) for v in name.(variables(d)))
 mean(d, s::Number) = mean(d, BlockWeighting(ntuple(i->s,embeddim(d))))
 
@@ -16,11 +16,11 @@ mean(d, s::Number) = mean(d, BlockWeighting(ntuple(i->s,embeddim(d))))
 Spatial mean of spatial data `sdata`. Optionally,
 specify the variable `v` and the block side `s`.
 """
-mean(d::Data) = mean(d, median_heuristic(d))
+mean(d::Data) = mean(d, mode_heuristic(d))
 
 var(d, v::Symbol, w::WeightingMethod) = var(d[v], weight(d, w), mean=mean(d, v, w), corrected=false)
 var(d, v::Symbol, s::Number) = var(d, v, BlockWeighting(ntuple(i->s,embeddim(d))))
-var(d, v::Symbol) = var(d, v, median_heuristic(d))
+var(d, v::Symbol) = var(d, v, mode_heuristic(d))
 var(d, w::WeightingMethod) = Dict(v => var(d, v, w) for v in name.(variables(d)))
 var(d, s::Number) = var(d, BlockWeighting(ntuple(i->s,embeddim(d))))
 
@@ -32,11 +32,11 @@ var(d, s::Number) = var(d, BlockWeighting(ntuple(i->s,embeddim(d))))
 Spatial variance of spatial data `sdata`. Optionally,
 specify the variable `v` and the block side `s`.
 """
-var(d::Data) = var(d, median_heuristic(d))
+var(d::Data) = var(d, mode_heuristic(d))
 
 quantile(d, v::Symbol, p, w::WeightingMethod) = quantile(d[v], weight(d, w), p)
 quantile(d, v::Symbol, p, s::Number) = quantile(d, v, p, BlockWeighting(ntuple(i->s,embeddim(d))))
-quantile(d, v::Symbol, p) = quantile(d, v, p, median_heuristic(d))
+quantile(d, v::Symbol, p) = quantile(d, v, p, mode_heuristic(d))
 quantile(d, p, w::WeightingMethod) = Dict(v => quantile(d, v, p, w) for v in name.(variables(d)))
 quantile(d, p::T, s::Number) where {T<:Union{Number,AbstractVector}} = quantile(d, p, BlockWeighting(ntuple(i->s,embeddim(d))))
 
@@ -48,14 +48,14 @@ quantile(d, p::T, s::Number) where {T<:Union{Number,AbstractVector}} = quantile(
 Spatial quantile of spatial data `sdata` at probability `p`.
 Optionally, specify the variable `v` and the block side `s`.
 """
-quantile(d::Data, p) = quantile(d, p, median_heuristic(d))
+quantile(d::Data, p) = quantile(d, p, mode_heuristic(d))
 
 
 function dist_matrix_random_sample(d, npoints=1000)
 
   # select at most 1000 points at random
   nel = nelements(d)
-  inds = sample(1:nel, min(nel, 1000), replace=false)
+  inds = sample(1:nel, min(nel, npoints), replace=false)
   X = (coordinates(centroid(d, ind)) for ind in inds)
   pairwise(Euclidean(), X)
 end
@@ -63,7 +63,6 @@ end
 function median_heuristic(d)
 
   D = dist_matrix_random_sample(d)
-
   # median heuristic
   n = size(D, 1)
   m = median(D[i,j] for i in 1:n for j in 1:n if i > j)
