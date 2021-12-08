aee610a675fe7c74560abc090d5bad1b1d35d664
Franco Naghetini <franconaghetini@gmail.com>
2021-12-08 19:20:08 -0300

    Add Vulcan convention

--- src/rotations.jl
+++ src/rotations.jl
@@ -3,25 +3,45 @@
 # ------------------------------------------------------------------
 
 """
-    Datamine(α, β, θ)
+    DatamineAngles(α, β, θ)
 
 Datamine ZXY rotation convention following the left-hand rule.
 All angles are in degrees and the signal convention is CW, CW, CW
-positive.
+positive. Y is the principal axis.
 """
 
-function Datamine(α, β, θ)
+function DatamineAngles(α, β, θ)
     EulerAngles(-deg2rad(α - 90), deg2rad(β), -deg2rad(θ), :ZYX)
 end
 
 """
-    Gslib(α, β, θ)
+    GslibAngles(α, β, θ)
 
 GSLIB ZXY rotation convention following the left-hand rule.
 All angles are in degrees and the signal convention is CW, CCW, CCW
-positive.
+positive. Y is the principal axis.
+
+## Reference
+* Deutsch, 2015. [The Angle Specification for GSLIB Software]
+(https://geostatisticslessons.com/lessons/anglespecification)
 """
 
-function Gslib(α, β, θ)
+function GslibAngles(α, β, θ)
     EulerAngles(-deg2rad(α - 90), -deg2rad(β), deg2rad(θ), :ZYX)
+end
+
+"""
+    VulcanAngles(α, β, θ)
+
+GSLIB ZYX rotation convention following the right-hand rule.
+All angles are in degrees and the signal convention is CW, CCW, CW
+positive. X is the principal axis.
+
+## Reference
+* Deutsch, 2015. [The Angle Specification for GSLIB Software]
+(https://geostatisticslessons.com/lessons/anglespecification)
+"""
+
+function VulcanAngles(α, β, θ)
+    EulerAngles(-deg2rad(α - 90), -deg2rad(β), -deg2rad(θ), :ZYX)
 end
