e23c1d9fd3ea68d3d500f0dfeb1045c8bd6c43c5
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-09-18 17:28:25 -0300

    More fixes for transforms.jl

--- src/transforms.jl
+++ src/transforms.jl
@@ -50,9 +50,28 @@ end
 # --------------------------------------------------
 
 function applymeta(::Sample, dom::Domain, prep)
-  sinds = prep
+  sinds, rinds = prep
 
   sdom = view(dom, sinds)
+  rdom = view(dom, rinds)
 
-  sdom, nothing
+  sdom, (sinds, rinds, rdom)
+end
+
+function revertmeta(::Sample, newdom::Domain, mcache)
+  geoms = collect(newdom)
+
+  sinds, rinds, rdom = mcache
+
+  uinds  = sort(unique(sinds))
+  ugeoms = map(uinds) do i
+    j = findfirst(==(i), sinds)
+    geoms[j]
+  end
+
+  for (i, geom) in zip(rinds, rdom)
+    insert!(ugeoms, i, geom)
+  end
+
+  Collection(ugeoms)
 end
