e80b38b8fc3bdeb9370ed005e30cdd5000f1d165
Elias Carvalho <eliascarvdev@gmail.com>
2022-11-27 11:26:23 -0300

    Update @transfor end @combine docstrings

--- src/geoops/transform.jl
+++ src/geoops/transform.jl
@@ -10,8 +10,9 @@ and new columns `col₁`, `col₂`, ..., `colₙ` defined by expressions
 `expr₁`, `expr₂`, ..., `exprₙ`. The `object` can be a `Data` object
 or a `Partition` object returned by the `@groupby` macro.
 In each expression the `object` columns are represented by symbols 
-and the functions use `broadcast` by default. If there are columns in the table 
-with the same name as the new columns, these will be replaced.
+and the functions use `broadcast` by default. Also, is pissible pass strings 
+or variables as column names using the `{colname}` syntax. If there are columns 
+in the table with the same name as the new columns, these will be replaced.
 
 See also: [`@groupby`](@ref).
 
@@ -25,6 +26,10 @@ See also: [`@groupby`](@ref).
 p = @groupby(data, :y)
 @transform(p, :logx = log(:x))
 @transform(p, :expz = exp(:z))
+
+@transform(data, {"z"} = {"x"} - 2*{"y"})
+xnm, ynm, znm = :x, :y, :z
+@transform(data, {znm} = {xnm} - 2*{ynm})
 ```
 
 ### Notes
