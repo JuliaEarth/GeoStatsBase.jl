413059b04a8528c4255ecc08bee09fea588a0ff9
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-06-22 10:50:24 -0300

    Refactor rotations.jl

--- src/rotations.jl
+++ src/rotations.jl
@@ -12,27 +12,24 @@ positive. Y is the principal axis.
 DatamineAngles(α, β, θ) = RotZYX(deg2rad(θ), -deg2rad(β), deg2rad(α - 90))
 
 """
+    VulcanAngles(α, β, θ)
+
+GSLIB ZYX rotation convention following the right-hand rule.
+All angles are in degrees and the signal convention is CW, CCW, CW
+positive. X is the principal axis.
+"""
+VulcanAngles(α, β, θ) = RotZYX(deg2rad(θ), deg2rad(β), deg2rad(α - 90))
+
+"""
     GslibAngles(α, β, θ)
 
 GSLIB ZXY rotation convention following the left-hand rule.
 All angles are in degrees and the signal convention is CW, CCW, CCW
 positive. Y is the principal axis.
 
-## Reference
+## References
+
 * Deutsch, 2015. [The Angle Specification for GSLIB Software]
-(https://geostatisticslessons.com/lessons/anglespecification)
+  (https://geostatisticslessons.com/lessons/anglespecification)
 """
 GslibAngles(α, β, θ) = RotZYX(-deg2rad(θ), deg2rad(β), deg2rad(α - 90))
-
-"""
-    VulcanAngles(α, β, θ)
-
-GSLIB ZYX rotation convention following the right-hand rule.
-All angles are in degrees and the signal convention is CW, CCW, CW
-positive. X is the principal axis.
-
-## Reference
-* Deutsch, 2015. [The Angle Specification for GSLIB Software]
-(https://geostatisticslessons.com/lessons/anglespecification)
-"""
-VulcanAngles(α, β, θ) = RotZYX(deg2rad(θ), deg2rad(β), deg2rad(α - 90))
