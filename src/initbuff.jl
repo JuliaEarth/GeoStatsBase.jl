4beb57ca0b9a3745ec990d818786bdd8934e573b
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-08-13 17:30:25 -0300

    Refactor initbuff.jl

--- src/initbuff.jl
+++ src/initbuff.jl
@@ -10,38 +10,32 @@ A method to initialize buffers in geostatistical solvers.
 abstract type InitMethod end
 
 """
-    initbuff(sdata, sdomain, vars, method)
+    initbuff(domain, vars, method; [data])
 
 Initialize buffers for all variables `vars` with given `method`
-based on the location of elements in `sdata` and `sdomain`.
+based on the location of elements in `domain` and (optionally) `data`.
 """
-function initbuff(sdata, sdomain, vars, method::InitMethod)
-  buff, mask = allocbuff(vars, nelements(sdomain))
+function initbuff(domain, vars, method::InitMethod; data=nothing)
+  names = keys(vars)
+  types = values(vars)
+  nelem = nelements(domain)
 
-  if !isnothing(sdata)
+  buff = Dict(var => Vector{V}(undef, nelem) for (var, V) in zip(names, types))
+  mask = Dict(var => falses(nelem) for var in names)
+
+  if !isnothing(data)
     ivars = keys(vars)
-    dvars = setdiff(propertynames(sdata), [:geometry])
-    preproc = preprocess(sdata, sdomain, method)
+    dvars = setdiff(propertynames(data), [:geometry])
+    preproc = preprocess(data, domain, method)
     for var in ivars ∩ dvars
-      initbuff!(buff[var], mask[var], valuesof(sdata, var), method, preproc)
+      table = values(data)
+      cols = Tables.columns(table)
+      vals = Tables.getcolumn(cols, var)
+      initbuff!(buff[var], mask[var], vals, method, preproc)
     end
   end
 
   buff, mask
-end
-
-function allocbuff(vars, n)
-  names = keys(vars)
-  types = values(vars)
-  buff = Dict(var => Vector{V}(undef, n) for (var, V) in zip(names, types))
-  mask = Dict(var => falses(n) for var in names)
-  buff, mask
-end
-
-function valuesof(sdata, var)
-  table = values(sdata)
-  cols = Tables.columns(table)
-  Tables.getcolumn(cols, var)
 end
 
 # ----------------
--- test/initbuff.jl
+++ test/initbuff.jl
@@ -4,21 +4,21 @@
 
   @testset "nearest" begin
     grid = CartesianGrid((100,), (-0.5,), (1.0,))
-    buff, mask = initbuff(data1D, grid, (; value=Float64), NearestInit())
+    buff, mask = initbuff(grid, (; value=Float64), NearestInit(), data=data1D)
     for (i, j) in (100 => 11, 81 => 9, 11 => 2, 21 => 3, 91 => 10, 51 => 6, 61 => 7, 71 => 8, 31 => 4, 41 => 5, 1 => 1)
       @test buff[:value][i] == data1D.value[j]
       @test mask[:value][i] == true
     end
 
     grid = CartesianGrid((100, 100), (-0.5, -0.5), (1.0, 1.0))
-    buff, mask = initbuff(data2D, grid, (; value=Float64), NearestInit())
+    buff, mask = initbuff(grid, (; value=Float64), NearestInit(), data=data2D)
     for (i, j) in (5076 => 3, 2526 => 1, 7551 => 2)
       @test buff[:value][i] == data2D.value[j]
       @test mask[:value][i] == true
     end
 
     pset = PointSet([25.0 50.0 75.0; 25.0 75.0 50.0])
-    buff, mask = initbuff(data2D, pset, (; value=Float64), NearestInit())
+    buff, mask = initbuff(pset, (; value=Float64), NearestInit(), data=data2D)
     for (i, j) in (2 => 2, 3 => 3, 1 => 1)
       @test buff[:value][i] == data2D.value[j]
       @test mask[:value][i] == true
@@ -30,21 +30,21 @@
     grid = CartesianGrid(10, 10)
 
     # copy data to first locations in domain
-    buff, mask = initbuff(data, grid, (; z=Float64), ExplicitInit())
+    buff, mask = initbuff(grid, (; z=Float64), ExplicitInit(), data=data)
     for (i, j) in (i => i for i in 1:10)
       @test buff[:z][i] == data.z[j]
       @test mask[:z][i] == true
     end
 
     # copy data to last locations in domain
-    buff, mask = initbuff(data, grid, (; z=Float64), ExplicitInit(91:100))
+    buff, mask = initbuff(grid, (; z=Float64), ExplicitInit(91:100), data=data)
     for (i, j) in (j => i for (i, j) in enumerate(91:100))
       @test buff[:z][i] == data.z[j]
       @test mask[:z][i] == true
     end
 
     # copy first 3 data points to last 3 domain locations
-    buff, mask = initbuff(data, grid, (; z=Float64), ExplicitInit(1:3, 98:100))
+    buff, mask = initbuff(grid, (; z=Float64), ExplicitInit(1:3, 98:100), data=data)
     for (i, j) in (j => i for (i, j) in enumerate(98:100))
       @test buff[:z][i] == data.z[j]
       @test mask[:z][i] == true
