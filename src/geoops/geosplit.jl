1518395ca11208ed6ee37b0575caf465d0560ce1
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2023-08-29 10:32:32 -0300

    Add the `geosplit` function (#153)
    * Add the 'geosplit' function
    
    * Update src/geoops/geosplit.jl
    
    ---------
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

---
+++ src/geoops/geosplit.jl
@@ -0,0 +1,14 @@
+"""
+    geosplit(object, fraction, [normal])
+
+Split geospatial `object` into two parts where the first
+part has a `fraction` of the elements. Optionally, the
+split is performed perpendicular to a `normal` direction.
+"""
+function geosplit(object, fraction::Real, normal=nothing)
+  if isnothing(normal)
+    partition(object, FractionPartition(fraction))
+  else
+    partition(object, BisectFractionPartition(normal, fraction))
+  end
+end
