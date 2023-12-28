c7adec8d08d5a4e3bcf4452716eb912fe3276708
Elias Carvalho <eliascarvdev@gmail.com>
2023-12-28 17:57:07 -0300

    Fix 'GslibAngles' implementation

--- test/rotations.jl
+++ test/rotations.jl
@@ -8,32 +8,31 @@
   @test datamine * v₁ ≈ [3.0810411262386967, 1.2425629737653001, 1.7214014158973259]
   @test datamine * v₂ ≈ [0.7601180732526871, 0.5597366702348654, 0.09442126195411826]
 
-  # GSLIB convention
-  gslib = GslibAngles(70, -30, 20)
-
-  @test gslib * v₁ ≈ [3.0810411262386967, 1.2425629737653001, 1.7214014158973259]
-  @test gslib * v₂ ≈ [0.7601180732526871, 0.5597366702348654, 0.09442126195411826]
-
   # Vulcan convention
   vulcan = VulcanAngles(70, -30, -20)
 
   @test vulcan * v₁ ≈ [3.0810411262386967, 1.2425629737653001, 1.7214014158973259]
   @test vulcan * v₂ ≈ [0.7601180732526871, 0.5597366702348654, 0.09442126195411826]
 
-  # MineSight convention
-  minisight = MinesightAngles(30, 15, -15)
+  # GSLIB convention
+  gslib = GslibAngles(30, 15, -15)
 
-  @test minisight * v₁ ≈ [3.7410191941252595, -0.034675177060507156, 0.059774754555877996]
-  @test minisight * v₂ ≈ [0.8592918193713616, 0.4018496819077273, -0.011593201115905286]
+  @test gslib * v₁ ≈ [3.7410191941252595, -0.034675177060507156, 0.059774754555877996]
+  @test gslib * v₂ ≈ [0.8592918193713616, 0.4018496819077273, -0.011593201115905286]
+
+  # MineSight convention
+  minesight = MinesightAngles(30, 15, -15)
+
+  @test minesight * v₁ ≈ [3.7410191941252595, -0.034675177060507156, 0.059774754555877996]
+  @test minesight * v₂ ≈ [0.8592918193713616, 0.4018496819077273, -0.011593201115905286]
 
   # comparing conventions
-  @test datamine ≈ gslib
   @test datamine ≈ vulcan
-  @test gslib ≈ vulcan
+  @test gslib ≈ minesight
 
   # rotation conversion
   rot = RotZXY(0.1, 0.2, 0.3)
-  @test DatamineAngles(rot) ≈ GslibAngles(rot) ≈ VulcanAngles(rot) ≈ rot
+  @test DatamineAngles(rot) ≈ VulcanAngles(rot) ≈ rot
 
   θ₁, θ₂, θ₃ = -15, 45, 30
   rot = RotZYX(deg2rad(θ₃), -deg2rad(θ₂), deg2rad(θ₁ - 90))
@@ -44,13 +43,11 @@
   vulcan = VulcanAngles(rot)
   @test Rotations.params(vulcan) ≈ [θ₁, θ₂, θ₃]
 
-  rot = RotZYX(-deg2rad(θ₃), deg2rad(θ₂), deg2rad(θ₁ - 90))
-  gslib = GslibAngles(rot)
-  @test Rotations.params(gslib) ≈ [θ₁, θ₂, θ₃]
-
   θ₁, θ₂, θ₃ = 30, 15, -15
   rot = RotYXZ(-deg2rad(θ₃), deg2rad(θ₂), -deg2rad(θ₁))
-  minisight = MinesightAngles(rot)
-  @test minisight ≈ rot
-  @test Rotations.params(minisight) ≈ [θ₁, θ₂, θ₃]
+  gslib = GslibAngles(rot)
+  minesight = MinesightAngles(rot)
+  @test gslib ≈ minesight ≈ rot
+  @test Rotations.params(gslib) ≈ [θ₁, θ₂, θ₃]
+  @test Rotations.params(minesight) ≈ [θ₁, θ₂, θ₃]
 end
